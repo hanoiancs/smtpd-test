bfae7a0dd5afaf2bfa190715574d45be7a72d409
Quan Tran Anh <quanta@ecomobi.com>
2021-04-16 16:41:02 +0700

    Add Next, Prev button

--- static/app.js
+++ static/app.js
@@ -7,12 +7,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
         },
         mounted: function () {
             axios
-              .get('/mails')
-              .then(response => (response.status === 200 ? this.mails = response.data : this.mails = []))
+                .get('/mails')
+                .then(response => (response.status === 200 ? this.mails = response.data : this.mails = []))
         },
         methods: {
             show: function (idx) {
                 this.current_mail = this.mails[idx];
+            },
+            fetch_next: function () {
+                console.log('fetch next');
+            },
+            fetch_prev: function () {
+                console.log('fetch prev');
             }
         }
     });

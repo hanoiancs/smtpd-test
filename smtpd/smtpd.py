180a856f9a6f55dd91b0885e48b163c4e0ad888d
Quan Tran Anh <hanoiancs@gmail.com>
2023-04-12 23:48:42 +0700

    Dockerize

--- smtpd/smtpd.py
+++ smtpd/smtpd.py
@@ -6,7 +6,7 @@ import asyncio
 import pymongo
 
 from dotenv import load_dotenv
-from email import message_from_bytes, message_from_string
+from email import message_from_bytes
 from aiosmtpd.controller import Controller
 from aiosmtpd.smtp import (
     SMTP,
@@ -26,8 +26,10 @@ client = pymongo.MongoClient(
     password=os.getenv("DB_MONGO_PASSWORD"),
     authSource=os.getenv("DB_MONGO_AUTHENTICATION_DATABASE")
 )
+
 db = client[os.getenv("DB_MONGO_DATABASE")]
 
+print(f"DB_MONGO_HOST: {os.getenv('DB_MONGO_HOST')}")
 
 def decode_mime_words(s):
     return u''.join(
@@ -35,28 +37,9 @@ def decode_mime_words(s):
         for word, encoding in email.header.decode_header(s))
 
 
-class ExampleHandler:
-    # async def handle_RCPT(self, server: SMTP, session: Session, envelope: Envelope, address, rcpt_options):
-        # if not address.endswith('@example.com'):
-        #     return '550 not relaying to that domain'
-
-        # envelope.rcpt_tos.append(address)
-        #
-        # return '250 OK'
-
+class MongoDbHandler:
     async def handle_DATA(self, server: SMTP, session: Session, envelope: Envelope):
         message = message_from_bytes(envelope.content)
-        # print('Message from %s' % envelope.mail_from)
-        # print('Message for %s' % envelope.rcpt_tos)
-        # print('Message subject `%s`' % message.get('Subject'))
-        # print('Message data:\n')
-        # # for ln in envelope.content.decode('utf', errors='replace').splitlines():
-        # #     print(f'> {ln}'.strip())
-        # # print(envelope.content.decode('utf', errors='replace'))
-        # print(message.get_payload())
-        # print()
-        # print('End of message')
-        # Insert into database
         db.mails.insert_one({
             "client_id": session.auth_data['id'],
             "from": envelope.mail_from,
@@ -70,7 +53,7 @@ class ExampleHandler:
 
 class Authenticator:
     def __init__(self):
-        self.auth_db = pymongo.MongoClient("localhost", 27017)
+        pass
 
     def __call__(self, server: SMTP, session: Session, envelope: Envelope, mechanism: str, auth_data):
         fail_nothandled = AuthResult(success=False, handled=False)
@@ -78,19 +61,24 @@ class Authenticator:
             return fail_nothandled
         if not isinstance(auth_data, LoginPassword):
             return fail_nothandled
+        
         username = auth_data.login.decode()
         password = auth_data.password.decode()
+
         auth_client = db.clients.find_one({
             "username": username,
             "password": password
         })
+
         if auth_client:
             return AuthResult(success=True, auth_data={"id": str(auth_client['_id'])})
+        
         return fail_nothandled
 
 
 async def amain(loop):
-    handler = ExampleHandler()
+    handler = MongoDbHandler()
+
     cont = Controller(
         handler,
         hostname=os.getenv('SMTP_SERVER_HOSTNAME'),
@@ -99,6 +87,7 @@ async def amain(loop):
         auth_required=os.getenv('SMTP_SERVER_AUTH_REQUIRED').lower() == 'true',
         auth_require_tls=os.getenv('SMTP_SERVER_AUTH_REQUIRE_TLS').lower() == 'true'
     )
+
     cont.start()
 
 
